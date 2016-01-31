6b1a645b43b7ad113cda99c09dc6b323e54af23b
WhyYouNotStudyForExam <mischaschev@web.de>
2016-01-31 05:02:31 +0100

    Scene Completed
    Menu

---
+++ Unity/The Voice Of Goat/Assets/Scripts/MusicButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicButton : MonoBehaviour {
+
+    public Sprite MusicOn;
+    public Sprite MusicOff;
+    bool isOn = true;
+
+    void OnMouseUp()
+    {
+        if(isOn)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = MusicOff;
+        }
+        else
+        {
+            
+            this.GetComponent<SpriteRenderer>().sprite = MusicOn;
+        }
+        isOn = !isOn;
+    }
+}
---
+++ Unity/The Voice Of Goat/Assets/Scripts/SoundButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundButton : MonoBehaviour {
+
+    public Sprite SoundOn;
+    public Sprite SoundOff;
+    bool isOn = true;
+
+    void OnMouseUp()
+    {
+        if (isOn)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = SoundOff;
+        }
+        else
+        {
+            this.GetComponent<SpriteRenderer>().sprite = SoundOn;
+        }
+        isOn = !isOn;
+    }
+}
